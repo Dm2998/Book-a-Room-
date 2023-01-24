ea8dad085789b50ec0d4ebb90af8727ec934d759
Diego Alexander <114578666+Dm2998@users.noreply.github.com>
2023-01-24 23:06:58 +0000

    Update form-validation.js

--- form-validation.js
+++ form-validation.js
@@ -1,10 +1,11 @@
 // // Example starter JavaScript for disabling form submissions if there are invalid fields
 // (() => {
 //   'use strict'
-
+  // validation for bootstrap 4
 //   // Fetch all the forms we want to apply custom Bootstrap validation styles to
 //   const forms = document.querySelectorAll('.needs-validation')
 
+  // array from forms
 //   // Loop over them and prevent submission
 //   Array.from(forms).forEach(form => {
 //     form.addEventListener('submit', event => {
@@ -13,6 +14,7 @@
 //         event.stopPropagation()
 //       }
 
+// add class to form
 //       form.classList.add('was-validated')
 //     }, false)
 //   })
